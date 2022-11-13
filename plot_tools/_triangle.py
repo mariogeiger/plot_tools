8ecf8e878d780b245452e1fde1ab018c1c15ca1c
Mario Geiger <geiger.mario@gmail.com>
2022-11-12 20:57:02 -0500

    update log log triangle

--- plot_tools/_triangle.py
+++ plot_tools/_triangle.py
@@ -2,17 +2,29 @@ import math
 import matplotlib.pyplot as plt
 
 
-def log_log_triangle(x1, x2, y1, y2=None, slope=None, text=None):
+def log_log_triangle(
+    x1: float,
+    x2: float,
+    y1: float,
+    y2: float = None,
+    *,
+    slope: float = None,
+    text: str = None,
+):
+    if y2 is None and slope is None:
+        raise ValueError("Either y2 or slope must be specified")
+
     if y2 is None:
-        y2 = y1 * (x2 / x1)**slope
+        y2 = y1 * (x2 / x1) ** slope
 
     if text is None:
-        text = math.log(y2 / y1) / math.log(x2 / x1)
+        text = f"{math.log(y2 / y1) / math.log(x2 / x1):.2f}"
 
-    plt.plot([x1, x2, x2, x1], [y1, y2, y1, y1], 'k-')
+    plt.plot([x1, x2, x2, x1], [y1, y2, y1, y1], "k-")
+
     plt.annotate(
         text,
-        ((x1*x2**2)**(1/3), (y1**2*y2)**(1/3)),
-        horizontalalignment='center',
-        verticalalignment='center'
+        ((x1 * x2**2) ** (1 / 3), (y1**2 * y2) ** (1 / 3)),
+        horizontalalignment="center",
+        verticalalignment="center",
     )
